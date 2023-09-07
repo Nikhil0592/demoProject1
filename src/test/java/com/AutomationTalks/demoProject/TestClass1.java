b7434824888fdf1c653e22d4efd667e22d77856c
Nikhil Arora <aroranikhil593@gmail.com>
2023-09-07 17:36:39 +0530

    first commit

---
+++ src/test/java/com/AutomationTalks/demoProject/TestClass1.java
@@ -0,0 +1,51 @@
+package com.AutomationTalks.demoProject;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+	
+	public static WebDriver driver;
+
+	@SuppressWarnings("deprecation")
+	@BeforeMethod
+	public void launchDriver() {
+		
+		
+		driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().fullscreen();
+		
+		
+	}
+	@Test
+	public void Test1() {
+		
+		driver.navigate().to("https://rahulshettyacademy.com/AutomationPractice/");
+		System.out.println("Test 1 title is "+ driver.getTitle());
+	}
+	@Test
+public void Test2() {
+	driver.navigate().to("https://rahulshettyacademy.com/AutomationPractice/");
+	System.out.println("Test 2 title is "+ driver.getTitle());
+		
+	}
+	
+@Test
+public void Test3() {
+	driver.navigate().to("https://rahulshettyacademy.com/AutomationPractice/");
+	System.out.println("Test 3 title is "+ driver.getTitle());
+	
+}
+@AfterMethod
+public void quit() {
+	driver.quit();
+	
+}
+
+}
